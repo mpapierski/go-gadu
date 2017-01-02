33d026d47dad56f139f0c1b4bbcc47d76a5fdac0
Michał Papierski <michal@papierski.net>
2017-01-02 22:43:33 +0100

    Implemented a ping ticker.

--- gadu.go
+++ gadu.go
@@ -13,9 +13,18 @@ static struct gg_session* gg_login2(struct gg_login_params* params, int* error) 
 	}
 	return session;
 }
+
+int gg_ping2(struct gg_session* session) {
+	if (gg_ping(session) == -1) {
+		return errno;
+	}
+	return 0;
+}
 */
 import "C"
 import "unsafe"
+import "time"
+import "log"
 
 const (
 	// GGFeatureImageDescr means that client supports graphical statuses
@@ -33,7 +42,8 @@ type GGSession struct {
 	Password string
 	session  *C.struct_gg_session
 
-	events chan *GGEvent
+	events     chan *GGEvent
+	pingTicker *time.Ticker
 }
 
 // NewGGSession returns new instance of a session
@@ -41,6 +51,7 @@ func NewGGSession() *GGSession {
 	s := new(GGSession)
 	s.session = nil
 	s.events = make(chan *GGEvent, 100)
+	s.pingTicker = nil
 	return s
 }
 
@@ -53,6 +64,9 @@ func Version() string {
 func (session GGSession) Close() {
 	C.gg_free_session(session.session)
 	session.session = nil
+	if session.pingTicker != nil {
+		session.pingTicker.Stop()
+	}
 }
 
 func (session GGSession) watchFd() *GGEvent {
@@ -84,10 +98,29 @@ func (session GGSession) Login() error {
 			return AccessDeniedError
 		}
 		if e.Type() == GGEventConnectionSuccess {
+			// Start ping timer
+			session.pingTicker = time.NewTicker(60 * time.Second)
+			go session.ticker()
 			return nil
 		}
 	}
 	return Fault
+}
+
+func (session GGSession) ping() error {
+	if result := C.gg_ping2(session.session); result != 0 {
+		return NewGGError((int)(result))
+	}
+	return nil
+}
+
+func (session GGSession) ticker() {
+	for range session.pingTicker.C {
+		if err := session.ping(); err != nil {
+			log.Fatalf("Unable to send ping: %s", err)
+			break
+		}
+	}
 }
 
 // Logout ends connection with the server
